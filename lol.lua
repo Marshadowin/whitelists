b54d9497e09ad1b764ad8b8028cbf65a24d39fd7
Marsh <43791401+Marshadowin@users.noreply.github.com>
2022-12-31 18:30:51 -0500

    Create lol.lua

---
+++ lol.lua
@@ -0,0 +1,10 @@
+return {
+    ["bfV91U28At"] = true,
+    ["9YT0cFbaVy"] = true,
+    ["u1T5HJ9sUE"] = true,
+    ["NDfPfLT1ex"] = true,
+    ["lxJ4WXMP7Q"] = true,
+    ["r50do7opYb"] = true,
+    ["EtlV2WkpUf"] = true,
+    ["GVn9Phylv9"] = true,
+}
