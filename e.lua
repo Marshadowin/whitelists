d27e0eb9bb4eff37ca4fee2bf98c1f0901df5d86
Marsh <43791401+Marshadowin@users.noreply.github.com>
2022-12-31 18:56:37 -0500

    Create e.lua

---
+++ e.lua
@@ -0,0 +1,15 @@
+    pcall(function()
+        local hook; hook = hookmetamethod(game, "__namecall", function(Self,...)
+            if not checkcaller() then
+                local getname = getnamecallmethod()
+                local args = {...}
+        
+                if getgenv().NoFall then
+                if tostring(getname) == "FireServer" and getcallingscript().Name == "WorldClient" and type(args[1]) == "number" and type(args[2]) == "boolean" then
+                    return args[1] == 0.1
+                    end
+                    end
+                end
+            return hook(Self,...)
+        end)
+    end)
