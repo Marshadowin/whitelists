3b0f8be68c2ae4e16df010bdc83e4dac04410e87
Marsh <43791401+Marshadowin@users.noreply.github.com>
2023-03-13 22:55:24 -0400

    Update hwidgrabber.lua

--- hwidgrabber.lua
+++ hwidgrabber.lua
@@ -1,35 +1,33 @@
-function getHwid() 
-    local req = request or (syn and syn.request) or (Krnl and Krnl.request) or (fluxus and fluxus.request)
-    local hwid = req({
-       Url = "https://httpbin.org/get",
-       Method = "GET",
-       Headers = {
-           ["Syn-Fingerprint"] = ""
-       }
-    })
-    function getHwid(b)
-        headers = b.headers
-        for i, v in pairs(headers) do
-            if string.match(i:lower(),"print") or string.match(i:lower(),"identifier") then
-                return v
-            end
-        end
-        return game:GetService("RbxAnalyticsService").GetClientId()
-    end
-    if not hwid.Success then
-       return game:GetService("RbxAnalyticsService").GetClientId()
-    end
-    
-    if hwid.Success then
-       local data = game:GetService("HttpService"):JSONDecode(hwid.Body)
-        return getHwid(data)
-    end
-    return nil
-end
-local hwid = getHwid();
-    local clipBoard = setclipboard or toclipboard or set_clipboard or (Clipboard and Clipboard.set)
-    clipBoard(hwid)
-task.wait(1)
 pcall(function()
-      game.Players.LocalPlayer:Kick("hwid grabbed. sent to ur clipboard: " .. hwid)
+	function getHwid() 
+		local req = request or (syn and syn.request) or (Krnl and Krnl.request) or (fluxus and fluxus.request)
+		local hwid = req({
+		   Url = "https://httpbin.org/get",
+		   Method = "GET",
+		   Headers = {
+			   ["Syn-Fingerprint"] = ""
+		   }
+		})
+		function getHwid(sex)
+			headers = sex.headers
+			for i, v in pairs(headers) do
+				if string.match(i:lower(),"print") or string.match(i:lower(),"identifier") then
+					return v
+				end
+			end
+			return game:GetService("RbxAnalyticsService"):GetClientId()
+		end
+		if not hwid.Success then
+		   return game:GetService("RbxAnalyticsService"):GetClientId()
+		end
+		
+		if hwid.Success then
+		   local data = game:GetService("HttpService"):JSONDecode(hwid.Body)
+			return getHwid(data)
+		end
+		return nil
+	end
+	local hwid = getHwid()
+        setclipboard(hwid)
+        game.Players.LocalPlayer:Kick("Hwid is:  " .. hwid .. " We have copyed it to your clipboard!")
 end)
